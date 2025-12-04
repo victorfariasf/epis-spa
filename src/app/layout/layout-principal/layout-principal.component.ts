e1169806d953e429c055028d5f6bde52102d89c8
Victor <victorfariasfer@gmail.com>
2025-12-03 23:59:10 -0300

    feature: adicionado modificações do alto contraste para acontecer no layout. Layout adicionado

--- src/app/layout/layout-principal/layout-principal.component.ts
+++ src/app/layout/layout-principal/layout-principal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-layout-principal',
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LayoutPrincipalComponent implements OnInit {
 
-  constructor() { }
+  public highContrast: boolean = false;
+
+  constructor(private renderer: Renderer2, private el: ElementRef) { }
 
   ngOnInit(): void {
+  }
+
+  toggleContrast(state: boolean) {
+    this.highContrast = state;
+    if (state) {
+       document.body.classList.add('high-contrast');
+      this.renderer.addClass(this.el.nativeElement, 'high-contrast');
+    } else {
+      document.body.classList.remove('high-contrast');
+      this.renderer.removeClass(this.el.nativeElement, 'high-contrast');
+    }
+    localStorage.setItem('highContrast', String(state));
   }
 
 }
